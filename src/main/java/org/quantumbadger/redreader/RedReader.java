872b6eb9548306c442cc0a2bf749c20d91ddd0c3
Alejandro Dorado <a.dorado160@uniandes.edu.co>
2019-11-28 09:02:12 -0500

    It is reading the file

--- src/main/java/org/quantumbadger/redreader/RedReader.java
+++ src/main/java/org/quantumbadger/redreader/RedReader.java
@@ -29,8 +29,12 @@ import org.quantumbadger.redreader.io.RedditChangeDataIO;
 import org.quantumbadger.redreader.receivers.NewMessageChecker;
 import org.quantumbadger.redreader.reddit.prepared.RedditChangeDataManager;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Map;
 import java.util.UUID;
@@ -101,6 +105,17 @@ public class RedReader extends Application {
 		handler.post(new Runnable() {
 			@Override
 			public void run() {
+				try {
+					final String TAGFile = "TAPFile";
+					InputStream myfile = getAssets().open("myfile");
+					BufferedReader reader = new BufferedReader(new InputStreamReader(myfile));
+					String readline="";
+					while ((readline = reader.readLine()) != null){
+						Log.i(TAGFile, readline+"\n");
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 				final String TAG = "TAP";
 				Map<Thread, StackTraceElement[]> allStackTraces = Thread.getAllStackTraces();
 				String allStackTrace = "";
